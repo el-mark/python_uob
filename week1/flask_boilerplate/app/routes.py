0921f10a9443de85c18da3c16b853ebeba3a3c8e
Mark Harmsen <mharmsenr@gmail.com>
2024-01-24 16:13:12 +0000

    data on boilerplate plus date time example

--- week1/flask_boilerplate/app/routes.py
+++ week1/flask_boilerplate/app/routes.py
@@ -1,6 +1,12 @@
 from app import app
 from flask import render_template
 
+with app.open_resource('data/quotes.txt') as file:
+    quotes = []
+    for line in file:
+        quotes.append(line.decode("utf-8"))
+    app.globals_quotes = quotes
+
 @app.route('/')
 def home():
     return 'hello world'
@@ -8,4 +14,4 @@ def home():
 @app.route('/template_a')
 def template_a():
     variable_a = 'Variable A'
-    return render_template('template_a.html', variable_a=variable_a)
+    return render_template('template_a.html', variable_a=variable_a, quotes=app.globals_quotes)
