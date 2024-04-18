34d9d34ea6c756279ed11da4403a944717961c0d
Mark Harmsen <mharmsenr@gmail.com>
2024-04-18 16:31:00 +0100

    week10 code and repeated week9 for working un file upload

---
+++ week10/09-login-uploadfile/app/views.py
@@ -0,0 +1,118 @@
+from flask import render_template, redirect, url_for, flash, request
+from app import app, db
+from datetime import datetime
+from app.forms import LoginForm, RegistrationForm, AddStudentForm, BorrowForm, DeactivateStudentForm
+from app.models import Student, Loan, User
+from flask_login import current_user, login_user, logout_user, login_required
+from urllib.parse import urlsplit
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/datetime')
+def date_time():
+    now = datetime.now()
+    return render_template('datetime.html', title='Date & Time', now=now)
+
+@app.route('/listStudents')
+@login_required
+def listStudents():
+    students = Student.query.all()
+    return render_template('listStudents.html', title='List Students', students=students)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password', 'danger')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        flash(f'Login for {form.username.data}', 'success')
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Registration for {form.username.data} received', 'success')
+        return redirect(url_for('index'))
+    return render_template('registration.html', title='Register', form=form)
+
+
+@app.route('/add_student', methods=['GET', 'POST'])
+@login_required
+def add_student():
+    form = AddStudentForm()
+    if form.validate_on_submit():
+        new_student = Student(
+            username=form.username.data, firstname=form.firstname.data,
+            lastname=form.lastname.data, email=form.email.data
+            )
+        db.session.add(new_student)
+        try:
+            db.session.commit()
+            flash(f'New Student added: {form.username.data} received', 'success')
+            return redirect(url_for('index'))
+        except:
+            db.session.rollback()
+            if Student.query.filter_by(username=form.username.data).first():
+                form.username.errors.append('This username is already taken. Please choose another')
+            if Student.query.filter_by(email=form.email.data).first():
+                form.email.errors.append('This email address is already registered. Please choose another')
+    return render_template('add_student.html', title='Add Student', form=form)
+
+
+@app.route('/borrow', methods=['GET', 'POST'])
+@login_required
+def borrow():
+    form = BorrowForm()
+    if form.validate_on_submit():
+        new_loan = Loan(device_id=form.device_id.data,
+                        student_id=form.student_id.data,
+                        borrowdatetime=datetime.now())
+
+        db.session.add(new_loan)
+        try:
+            db.session.commit()
+            flash(f'New Loan added', 'success')
+            return redirect(url_for('index'))
+        except:
+            db.session.rollback()
+    return render_template('borrow.html', title='Borrow', form=form)
+
+
+@app.route('/deactivate', methods=['GET', 'POST'])
+@login_required
+def deactivateStudent():
+    form = DeactivateStudentForm()
+    if form.validate_on_submit():
+        student = Student.query.get(form.student_id.data)
+        student.active = False
+        db.session.add(student)
+        try:
+            db.session.commit()
+            flash(f'student {student} deactivated', 'success')
+            return redirect(url_for('index'))
+        except:
+            db.session.rollback()
+    return render_template('deactivateStudent.html', title='Deactivate Student', form=form)
+
+
